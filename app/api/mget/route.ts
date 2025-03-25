4049191b30378b2187791d3dfa21bee8a48f0205
Alexander Grigoriev <avgrigorev@hse.ru>
2025-03-25 11:53:16 +0300

    get

--- app/api/mget/route.ts
+++ app/api/mget/route.ts
@@ -2,9 +2,12 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
     const defaultString = 'http://localhost:3000'
+    // @ts-ignore
     const remoteUrl: string  = request.nextUrl.searchParams.get('remoteUrl')
     if(!remoteUrl) return NextResponse.json({ method: 'GET', result: {}, errors: ['no remoteUrl'] }, { status: 200 });
+    // @ts-ignore
     const host: string = request.nextUrl.searchParams.get('host') ? request.nextUrl.searchParams.get('host') : defaultString
+    // @ts-ignore
     const referer: string = request.nextUrl.searchParams.get('referer') ? request.nextUrl.searchParams.get('referer') : defaultString
     const response = await fetch(remoteUrl, {
         method: 'GET',
